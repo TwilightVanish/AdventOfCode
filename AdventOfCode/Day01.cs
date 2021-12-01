a510c36960fac820da81ee48f8e7bdc739c7abf3
TwilightVanish <jayson.koremans@gmail.com>
2021-12-01 13:57:19 +0100

    Added 2021 - day 1

---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public sealed class Day01 : BaseDay
+    {
+        private readonly int[] _input;
+
+        public Day01()
+        {
+            _input = File.ReadAllText(InputFilePath).Split("\n").Select(int.Parse).ToArray();
+        }
+
+        public override ValueTask<string> Solve_1() => new(CountDepthIncrements(1).ToString());
+        public override ValueTask<string> Solve_2() => new(CountDepthIncrements(3).ToString());
+
+        private int CountDepthIncrements(int slidingWindow)
+        {
+            var count = 0;
+
+            for (var i = slidingWindow; i < _input.Length; i++)
+                if (_input[i] > +_input[i - slidingWindow]) 
+                    count++;
+
+            return count;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,14 +1,19 @@
-﻿if (args.Length == 0)
-{
-    Solver.SolveLast();
-}
-else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
-{
-    Solver.SolveAll();
-}
-else
-{
-    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+﻿using System;
+using System.Linq;
 
-    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+switch (args.Length)
+{
+    case 0:
+        Solver.SolveLast();
+        break;
+    case 1 when args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase):
+        Solver.SolveAll();
+        break;
+    default:
+    {
+        var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+        Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+        break;
+    }
 }
