565c26b71e317e175781da1c33d0ce6fc46525c8
TwilightVanish <jayson.koremans@gmail.com>
2022-12-01 21:05:27 +0100

    Day 01, 2022 - Split off parsing

--- 2022/Day01.cs
+++ 2022/Day01.cs
@@ -5,19 +5,33 @@ namespace AdventOfCode._2022;
 
 public sealed class Day01 : BaseDay
 {
-  
-    public Day01() : base(1, 2022) {}
-    
-    public override void Parse() {}
+    private List<int> _elves;
+
+    public Day01() : base(1, 2022)
+    {
+        _elves = ParseElves();
+    }
+
+    public override void Parse() => ParseElves();
     public override string Part1() => GetRichElves(1).ToString();
     public override string Part2() => GetRichElves(3).ToString();
 
     private int GetRichElves(int count)
     {
+        var sum = 0;
+        for (var i = 0; i < count; i++)
+        {
+            sum += _elves[i];
+        }
+
+        return sum;
+    }
+
+    private List<int> ParseElves()
+    {
         var elves = new List<int>();
         var buffer = 0;
-
-        // Create elves
+        
         for (var i = 0; i < Input.Length; i++)
         {
             if (Input[i] == "")
@@ -29,16 +43,9 @@ public sealed class Day01 : BaseDay
             buffer += CustomParser.ParseInt(Input[i]);
         }
         
-        // Find richest
         elves.Sort();
         elves.Reverse();
 
-        var sum = 0;
-        for (var i = 0; i < count; i++)
-        {
-            sum += elves[i];
-        }
-
-        return sum;
+        return elves;
     }
 }
