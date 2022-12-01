170f3d02b069f2998b2d4bffeb0eaf4341e75457
TwilightVanish <jayson.koremans@gmail.com>
2022-12-01 13:58:53 +0100

    Added Day 01 of 2022

---
+++ 2022/Day01.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Utility;
+
+namespace AdventOfCode._2022;
+
+public sealed class Day01 : BaseDay
+{
+  
+    public Day01() : base(1, 2022) {}
+    
+    public override void Parse() {}
+    public override string Part1() => GetRichElves(1).ToString();
+    public override string Part2() => GetRichElves(3).ToString();
+
+    private int GetRichElves(int count)
+    {
+        var elves = new List<int>();
+        var buffer = 0;
+
+        // Create elves
+        for (var i = 0; i < Input.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(Input[i]))
+            {
+                elves.Add(buffer);
+                buffer = 0;
+            }
+
+            buffer += CustomParser.ParseInt(Input[i]);
+        }
+        
+        // Find richest
+        elves.Sort();
+        elves.Reverse();
+
+        var sum = 0;
+        for (var i = 0; i < count; i++)
+        {
+            sum += elves[i];
+        }
+
+        return sum;
+    }
+}
---
+++ Utility/CustomParser.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Utility;
+
+public static class CustomParser
+{
+    public static int ParseInt(string input)
+    {
+        var value = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            value = value*10 + (input[i] - '0');
+        }
+        return value;
+    }
+}
