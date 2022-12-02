7a8e8e2438ea49b88a933c9a583cc67a2040c3c4
TwilightVanish <jayson.koremans@gmail.com>
2022-12-02 12:09:51 +0100

    Added Day 2 of 2022

---
+++ 2022/Day02.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode._2022;
+
+public sealed class Day02 : BaseDay
+{
+    public Day02() : base(2, 2022)
+    {
+    }
+
+    public override void Parse() => Part1();
+    public override string Part1() => GetWrongPoints().ToString();
+    public override string Part2() => GetPoints().ToString();
+
+    private int GetWrongPoints()
+    {
+        int[][] scores =
+        {
+            new[] { 4, 8, 3 },
+            new[] { 1, 5, 9 },
+            new[] { 7, 2, 6 }
+        };
+        
+        var points = 0;
+        for (var i = 0; i < Input.Length; i++)
+        {
+            points += scores[Input[i][0] - 'A'][Input[i][2] - 'X'];
+        }
+
+        return points;
+    }
+
+    private int GetPoints()
+    {
+        int[][] scores =
+        {
+            new[] { 3, 4, 8 },
+            new[] { 1, 5, 9 },
+            new[] { 2, 6, 7 }
+        };
+        
+        var points = 0;
+        for (var i = 0; i < Input.Length; i++)
+        {
+            points += scores[Input[i][0] - 'A'][Input[i][2] - 'X'];
+        }
+
+        return points;
+    }
+}
