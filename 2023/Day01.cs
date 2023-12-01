4777a1a96a95ddda870f8cec892fad6c2bfeacb0
TwilightVanish <im.lisa.koremans@gmail.com>
2023-12-01 15:09:24 +0100

    Added day 01, 2023

---
+++ 2023/Day01.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode._2023;
+
+public sealed class Day01 : BaseDay
+{
+    public Day01() : base(1, 2023) { }
+
+    public override void Parse() { }
+    public override string Part1() => GetSum(RawInput).ToString();
+    public override string Part2() => FindActualCalibrationValues().ToString();
+
+    private int FindActualCalibrationValues()
+    {
+        string[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        string[] mask = {"o1e","t2o","t3e","f4r","f5e","s6x","s7n","e8t","n9e"};
+        
+        var masked = RawInput;
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            masked = masked.Replace(numbers[i], mask[i]);
+        }
+
+        return GetSum(masked);
+    }
+
+    private static int GetSum(string input)
+    {
+        var sum = 0;
+        
+        foreach (var line in input.AsSpan().EnumerateLines())
+        {
+            sum += (line[line.IndexOfAnyInRange('0', '9')] - '0') * 10;
+            sum += line[line.LastIndexOfAnyInRange('0', '9')] - '0';
+        }
+
+        return sum;
+    }
+}
--- 2023/README.md
+++ 2023/README.md
@@ -3,3 +3,8 @@
 - 13th Gen Intel Core i9-13900K, 1 CPU, 32 logical and 24 physical cores
 - Corsair Dual channel DDR5 5200 MHz 32GB 
 ---
+### Day 01
+| Method | Mean      | Error     | StdDev    | Gen0    | Allocated |
+|------- |----------:|----------:|----------:|--------:|----------:|
+| Part1  |  8.295 us | 0.0906 us | 0.0847 us |       - |      32 B |
+| Part2  | 57.123 us | 0.4452 us | 0.4165 us | 20.8130 |  392536 B |
