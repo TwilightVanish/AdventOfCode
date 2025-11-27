c7cb7ee0bc6cf2b60dec7017df5f45bbd92dd676
TwilightVanish <im.lisa.koremans@gmail.com>
2025-11-27 21:14:31 +0100

    Update to .NET 10 and add 2025

---
+++ 2025/Day01.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode._2025;
+
+public sealed class Day01 : BaseDay
+{
+    public Day01() : base(1, 2025) { }
+
+    public override void Parse() {}
+
+    public override string Part1() => "";
+    public override string Part2() => "";
+}
