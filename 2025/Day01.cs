a332e896c9d219c4e87f7f588f363bfea2858cbb
TwilightVanish <im.lisa.koremans@gmail.com>
2025-12-01 12:03:06 +0100

    Added Day 01, 2025

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -4,10 +4,73 @@ namespace AdventOfCode._2025;
 
 public sealed class Day01 : BaseDay
 {
-    public Day01() : base(1, 2025) { }
+    private const int Range = 100;
+    private readonly int[] _rotations;
+    
+    public Day01() : base(1, 2025)
+    {
+        _rotations = GetRotations();
+    }
 
-    public override void Parse() {}
+    public override void Parse() => GetRotations();
+    public override string Part1() => CountZeroPositions().ToString();
+    public override string Part2() => CountZeroClicks().ToString();
 
-    public override string Part1() => "";
-    public override string Part2() => "";
+    private int CountZeroPositions()
+    {
+        var counter = 0;
+        var position = 50;
+        
+        for (var i = 0; i < _rotations.Length; i++)
+        {
+            position += _rotations[i] % Range;
+            if (position < 0) position += Range;
+            if (position > Range - 1) position -= Range;
+            if (position == 0) counter++;
+        }
+
+        return counter;
+    }
+    
+    private int CountZeroClicks()
+    {
+        var counter = 0;
+        var position = 50;
+        
+        for (var i = 0; i < _rotations.Length; i++)
+        {
+            var startedOnZero = position == 0;
+
+            position += _rotations[i];
+            while (position < 0)
+            {
+                position += Range;
+                if (!startedOnZero) counter++;
+                startedOnZero = false;
+            }
+            
+            if (position == 0) counter++;
+
+            while (position > Range - 1)
+            {
+                position -= Range;
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    private int[] GetRotations()
+    {
+        var rotations = new int[Input.Length];
+        var i = 0;
+        foreach (var line in Input)
+        {
+            var direction = line[0] == 'L' ? -1 : 1;
+            rotations[i++] = Utility.CustomParser.ParseInt(line.AsSpan()[1..]) * direction;
+        }
+
+        return rotations;
+    }
 }
---
+++ 2025/README.md
@@ -0,0 +1,11 @@
+﻿# 2025 Benchmarks
+
+- 13th Gen Intel Core i9-13900K, 1 CPU, 32 logical and 24 physical cores
+- Corsair Dual channel DDR5 5200 MHz 32GB 
+---
+### Day 01
+| Method | Mean     | Error     | StdDev    | Gen0   | Allocated |
+|------- |---------:|----------:|----------:|-------:|----------:|
+| Part1  | 4.267 us | 0.0101 us | 0.0094 us |      - |      32 B |
+| Part2  | 5.643 us | 0.0157 us | 0.0147 us |      - |      32 B |
+| Parse  | 6.835 us | 0.0695 us | 0.0650 us | 0.9384 |   17752 B |
